e4f74f6db35d0ccdbf55ee0ea2e2ab33a6b308ca
Connor McCue <connor.mccue@comcast.net>
2022-08-03 00:32:00 -0700

    figured i should save before trying 2 arrays

--- README.md
+++ README.md
@@ -4,10 +4,20 @@
 
 If you work on more than one feature at a time, you are guaranteed to multiply your bugs and your anxiety.
 
-## Making a plan
+## The plan to make a poll maker
 
 1) **Make a drawing of your app. Simple "wireframes"** 
-1) **Look at the drawing and name the HTML elements you'll need to realize your vision**
+1) **section main**
+1) **div class left**
+1) **div class poll-container**
+1) **inputs: poll-question, option-a, option-b**
+1) **buttons: increase poll-a-votes, increase poll-b-votes, decrease poll-a-votes, decrease poll-b-votes**
+1) **div class right**
+1) **div class current-poll-container**
+1) **p-tag: poll-question, poll-a-votes, poll-b-votes**
+1) **section past polls**
+1) **DOM objects: each poll saved as array**
+1) **functions: renderPoll, displayPoll, savePoll,**
 1) **Look at the drawing and imagine using the app. What _state_ do you need to track?** 
 1) **For each HTML element ask: Why do I need this? (i.e., "we need div to display the results in")** 
 1) **Once we know _why_ we need each element, think about how to implement the "Why" as a "How" (i.e., `resultsEl.textContent = newResults`)**
--- app.js
+++ app.js
@@ -1,8 +1,153 @@
 // import functions and grab DOM elements
+import { renderPoll } from './render-utils.js';
 
-// let state
+const currentPollEl = document.getElementById('current-poll-container');
+const pastPollsEl = document.getElementById('past-poll-container');
+
+const currentQuestionInputEl = document.getElementById('question');
+const optionAInputEl = document.getElementById('option-a');
+const optionBInputEl = document.getElementById('option-b');
+
+const increaseOptionAButton = document.getElementById('increase-option-a');
+const increaseOptionBButton = document.getElementById('increase-option-b');
+const decreaseOptionAButton = document.getElementById('decrease-option-a');
+const decreaseOptionBButton = document.getElementById('decrease-option-b');
+const startPollButton = document.getElementById('start-poll');
+const savePollButton = document.getElementById('save-poll');
+
+
+
+// let state - done
+
+let question = '';
+let optionA = '';
+let optionB = '';
+let votesA = 0;
+let votesB = 0;
+let pastPolls = [];
+
+startPollButton.addEventListener('click', () => {
+    // get the name data from the form
+    const questionEl = currentQuestionInputEl.value;
+    const optionAEl = optionAInputEl.value;
+    const optionBEl = optionBInputEl.value;
+    // set the state to this data from the form
+    optionA = optionAEl;
+    optionB = optionBEl;
+    question = questionEl;
+    // reset the form values
+    currentQuestionInputEl.value = '';
+    optionAInputEl.value = '';
+    optionBInputEl.value = '';
+    // refresh the current game element with new data by calling the appropriate function
+    
+
+    console.log('start poll button pressed');
+    const newPoll = {
+        question: question,
+        optionA: optionA,
+        optionB: optionB,
+        votesA: votesA,
+        votesB: votesB,
+    };
+    pastPolls.push(newPoll);
+    pastPolls.textContent = '';
+    for (let poll of pastPolls) {
+        const pollEl = renderPoll(poll.question, poll.optionA, poll.optionB, poll.votesA, poll.votesB);
+        currentPollEl.append(pollEl); 
+
+    }
+    refreshCurrentPollEl();
+    question = '';
+    optionA = '';
+    optionB = '';
+    votesA = 0;
+    votesB = 0;
+
+    refreshCurrentPollEl();
+    displayAllPolls();
+});
+
+increaseOptionAButton.addEventListener('click', () => {
+    votesA++;
+    console.log(votesA);  
+    //refreshCurrentPoll();
+});
+increaseOptionBButton.addEventListener('click', () => {
+    votesB++;
+    console.log(votesB);  
+    //refreshCurrentPoll();
+});
+decreaseOptionAButton.addEventListener('click', () => {
+    votesA--;
+    console.log(votesA);  
+    //refreshCurrentPoll();
+});
+decreaseOptionBButton.addEventListener('click', () => {
+    votesB--;
+    console.log(votesB);  
+    //refreshCurrentPoll();
+});
+
+savePollButton.addEventListener('click', () => {
+    console.log('save poll button pressed');
+    const newPoll = {
+        question: question,
+        optionA: optionA,
+        optionB: optionB,
+        votesA: votesA,
+        votesB: votesB,
+    };
+
+    pastPolls.push(newPoll);
+    pastPolls.textContent = '';
+    for (let poll of pastPolls) {
+        const pollEl = renderPoll(poll.question, poll.optionA, poll.optionB, poll.votesA, poll.votesB);
+        pastPolls.append(pollEl); 
+
+    }
+    //refreshCurrentPoll();
+    question = '';
+    optionA = '';
+    optionB = '';
+    votesA = 0;
+    votesB = 0;
+
+    refreshCurrentPollEl();
+    displayAllPolls();
+});
+
+
+
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+function refreshCurrentPollEl() {
+    // clear the text contest of the question
+    currentPollEl.textContent = '';
+
+    currentQuestionInputEl.textContent = question;
+    optionAInputEl.textContent = optionA;
+    optionBInputEl.textContent = optionB;
+    
+
+    const pollEl = renderPoll(question, optionA, optionB, votesA, votesB);
+    pollEl.classList.add('current-poll-container');
+    currentPollEl.append(pollEl);
+
+}
+
+//function refreshCurrentQuestion() {
+    //currentQuestionEl.textContent = '';
+//}
+function displayAllPolls() {
+    currentPollEl.textContent = '';
+    pastPollsEl.textContent = '';
+    for (let poll of pastPolls) {
+        const pollEl = renderPoll(poll.question, poll.optionA, poll.optionB, poll.votesA, poll.votesB);
+        pastPollsEl.append(pollEl);
+    } 
+}
